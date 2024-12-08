6ea9c1e8f858c7f29acaf8b857cbb4405f7ea5f1
Ziyue Jin <ziyuejin877@gmail.com>
2024-12-08 16:01:28 -0500

    FEAT: Added logout button

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -8,9 +8,11 @@ import {
     NavBtnLink,
 } from "./NavbarElements";
 import LogIn from "../LogIn";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
     const [open, setOpen] = React.useState(false);
+    const navigate = useNavigate(); // For navigation after logout
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -18,6 +20,14 @@ const Navbar = () => {
 
     const handleClose = () => {
         setOpen(false);
+    };
+
+    const handleLogout = () => {
+        // Clear the JWT token from localStorage
+        localStorage.removeItem("authToken");
+
+        // Redirect the user to the login page
+        navigate("/login");
     };
 
     return (
@@ -34,15 +44,32 @@ const Navbar = () => {
                     <NavLink to="/Applications">
                         Track Applications
                     </NavLink>
-                    <NavLink to="/Profile"> {/* Add Profile Option */}
+                    <NavLink to="/Profile">
                         Profile
                     </NavLink>
                 </NavMenu>
                 <NavBtn>
+                    {/* Log In / Sign Up Button */}
                     <NavBtnLink to="/LogIn" onClick={handleClickOpen}>
                         Log In / Sign up
                     </NavBtnLink>
                     <LogIn open={open} handleClose={handleClose} />
+
+                    {/* Logout Button */}
+                    <button
+                        onClick={handleLogout}
+                        style={{
+                            background: "red",
+                            color: "white",
+                            border: "none",
+                            borderRadius: "5px",
+                            padding: "10px 20px",
+                            cursor: "pointer",
+                            marginLeft: "10px",
+                        }}
+                    >
+                        Log Out
+                    </button>
                 </NavBtn>
             </Nav>
         </>
